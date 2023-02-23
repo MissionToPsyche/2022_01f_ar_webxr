85e76a814e59e3166a05df40d3beefe06e7bd6bf
Noah Spott <ncs5113@psu.edu>
2023-02-22 22:59:51 -0500

    Simplify speech box & add educational information
    - simplify speech box - just made it so the css makes the speech box have the ability to scroll. It would be nice to have something to indicate to the user that there is more text
    
    - Added educational content from Cassie

--- PsycheProj/index.js
+++ PsycheProj/index.js
@@ -33,36 +33,80 @@ let currentNarrativeTextIterator = 0;
 let currentNarrativeTextSize = 0;
 
 // Narrative (const) text variables.
-const greeting = "Hello explorer!  I'm the Psyche satellite, here to guide you.  Look around and click the Place button " +
+const greeting = "Hello explorer!  I'm the Psyche spacecraft, here to guide you.  Look around and click the Place button " +
     "when the reticle is in the center of your screen."
 
 const modelDescriptions = [
-    "<Explain State 1 - It's (assumed) appearance 10 million years ago.>",
+    // Model 1 Description
+    "Stage 1: Formation of planetesimal",
+
+    // State Change 1 Description
     "<Explain transition from State 1 to State 2 - It accumulated particles over time.>",
-    "<Explain State 2 - It's (assumed) appearance 5 million years ago.>",
+
+    // Model 2 Description
+    "Stage 2: Planetesimal",
+
+    // State Change 2 Description
     "<Explain transition from State 2 to State 1 - It was broken down over time.>",
-    "<Explain State 3 - It's (assumed) appearance today.>"
+
+    // Model 3 Description
+    "Stage 3: Core/Current asteroid"
 ];
 
 const facts = [
+    // Model 1 Educational Information
     [
+        // Model 1 - Asteroid Button
         [
-            "<Model 1 - Fact 1> - This fact is gonna be really long to show the speech box behavior when there are long instances of text we wnat to display.  So, I'm gonna keep typing...",
-            "and showing that this can be really long.  Words words words.  These are a bunch of words.  Psyche is an asteroid.  It's really far away.  It's pretty cool.  It's so cool that NASA...",
-            "loves it.  This should be in the third speech box."
+            "Planetesimals are one of the building blocks of planets. The hypothesis that Psyche could potentially be leftover core material from a planetesimal could lead scientists to be able to investigate questions about Earth's core, including how it was formed."
         ],
-        "<Model 1 - Fact 2>",
-        "<Model 1 - Fact 3>"
+
+        // Model 1 - Question Button
+        [
+            "How might Psyche have formed?"
+        ],
+
+        // Model 1 - Spacecraft Button
+        [
+            "The spacecraft will is equipped with two Multispectral Imagers. These high resolution cameras will capture images of the asteroid's surface at different wavelengths of light. This, along with pictures of the topography of Psyche, will allow scientists to study features that provide clues to Psyche's history."
+        ]
     ],
+
+    // Model 2 Educational Information
     [
-        "<Model 2 - Fact 1>",
-        "<Model 2 - Fact 2>",
-        "<Model 2 - Fact 3>"
+        // Model 2 - Asteroid Button
+        [
+            "Scientists think Psyche may consist largely of metal from the core of a planetesimal, one of the building blocks of the rocky planets in our solar system (Mercury, Venus, Earth and Mars). Psyche is most likely a survivor of multiple violent hit-and-run collisions with other material, common when the solar system was forming."
+        ],
+
+        // Model 2 - Question Button
+        [
+            "How will it be determined if Psyche is core material of a planetesimal?"
+        ],
+        
+
+        // Model 2 - Spacecraft Button
+        [
+            "All of the instruments on the spacecraft will provide clues but, in particular, the magnetometer will look for evidence of an ancient magnetic field: if Psyche has a significant magnetic field still recorded in its solid body, it was once a core that produced its own dynamo."
+        ]
     ],
+
+    // Model 3 Educational Information
     [
-        "<Model 3 - Fact 1>",
-        "<Model 3 - Fact 2>",
-        "<Model 3 - Fact 3>"
+        // Model 3 - Asteroid Button
+        [
+            "After numerous collisions, it is hypothesized that the potential planetesimal would have its rocky mantle stripped away and leave behind the core material. This core material could potentially be what makes up the current asteroid Psyche." 
+        ],
+
+        // Model 3 - Question Button
+        [
+            "What is it that planetary cores are made of?"
+        ],
+
+        // Model 3 - Spacecraft Button
+        [
+            "The spacecraft contains a Gamma Ray and Neutron Spectrometer that will detect, measure, and map Psyche's elemental composition. These measurements will be able to give scientists a better idea of what exactly it is that potentially makes up the inner cores of planets."
+        ]
     ]
 ];
 
@@ -74,7 +118,7 @@ animate();
  * 
  * Initializes the AR experience.
  */
-$("#ARButton").click(async function() {
+$("#ARButton").click(/*async*/ function() {
     if(currentObject){
         currentObject.visible = false;
     }
@@ -142,7 +186,22 @@ $("#fact-three").click(function() {displayFact(3)});
  * @param {*} factNumber - Number (1-3) representing which fact to display.
  */
 function displayFact(factNumber) {
-    loadTextToNarrative(facts[currentModelState - 1][factNumber - 1]);
+    // Use switch to load correct string from 'facts' string array.
+    switch (currentModelState) {
+        case 1:
+            loadTextToNarrative(facts[0][factNumber - 1])
+            break;
+        case 2:
+            break;
+        case 3:
+            loadTextToNarrative(facts[1][factNumber - 1])
+            break;
+        case 4:
+            break;
+        case 5:
+            loadTextToNarrative(facts[2][factNumber - 1])
+            break;
+    }
 }
 
 /**
@@ -178,7 +237,9 @@ function unHideButtons() {
  * 
  * The loadTextToNarrative() function does not use the argument in this case.  So it can be anything (or empty string).
  */
+/*
 $("#speech-box-button").click(function() {loadTextToNarrative("This argument can be anything.")});
+*/
 
 /**
  * Change State button click.
@@ -294,7 +355,13 @@ function loadSatellite() {
  * Displays the speech box (narrative text).
  */
 function showNarrative() {
+    document.getElementById("textBox").style.display = "block";
     document.getElementById("narrative").style.display = "block";
+}
+
+function hideNarrative(){
+    document.getElementById("textBox").style.display = "none";
+    //document.getElementById("narrative").style.display = "none";
 }
 
 /**
@@ -314,6 +381,7 @@ function loadModelInfoToNarrative() {
  */
 function loadTextToNarrative(text) {
 
+    /*
     // Check if 'text' is a single string or an array of multiple strings.
     if (Array.isArray(text)) {
         // 'text' is an array of multiple strings - we need multiple speech boxes with the '...' button.
@@ -337,7 +405,7 @@ function loadTextToNarrative(text) {
         currentNarrativeTextIterator++;
 
         // Hide all buttons.
-        hideButtons();
+        //hideButtons();
         document.getElementById("place-button").style.display = "none";
 
     } else if (currentNarrativeTextArrayFlag == 1) {
@@ -352,7 +420,7 @@ function loadTextToNarrative(text) {
             currentNarrativeTextIterator = 0;
             currentNarrativeTextSize = 0;
             document.getElementById("speech-box-button").style.display = "none";
-            unHideButtons();
+            //unHideButtons();
         } else {
             // Increment the iterator.
             currentNarrativeTextIterator++;
@@ -365,8 +433,9 @@ function loadTextToNarrative(text) {
         currentNarrativeTextSize = 0;
         document.getElementById("speech-box-button").style.display = "none";
     }
+    */
 
-    document.getElementById("narrative").textContent = currentNarrativeText;
+    document.getElementById("narrative").textContent = text;
 }
 
 /**
@@ -451,6 +520,9 @@ function init() {
     // Create html element and add to container.
     container = document.createElement('div');
     document.getElementById("container").appendChild(container);
+
+    // Hide speech bubble
+    hideNarrative();
 
     // Initialize scene and camera.
     scene = new THREE.Scene();

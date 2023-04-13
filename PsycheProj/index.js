7ee9221a8de9ad6fe50b6725ba08304bca1ee7a8
Noah Spott <ncs5113@psu.edu>
2023-04-13 12:17:53 -0400

    Created webxr-not-supported.js
    - Moved code for webxr-not-supported.html into its own JS file

--- PsycheProj/index.js
+++ PsycheProj/index.js
@@ -21,12 +21,10 @@ let currentFactNumber;      // Number to track of the current fact number when a
 let menuDisp = false;
 const clock = new THREE.Clock();
 
-
 // File name variables.
 const globalMeshTexture = "pixel-rocks.png";
-const assets = "assets/";
 
-const introText = text.introText;
+// Variables for text from text.js
 const greeting = text.greeting;
 const modelDescriptions = text.modelDescriptions;
 const facts = text.facts;
@@ -45,35 +43,6 @@ window.addEventListener( "pageshow", function ( event ) {
 
 init();
 animate();
-
-$("#text-only-button").click(async function(){
-
-    // load text-version
-    window.location.replace("text-version.html");
-})
-
-$("#download-xrviewer-button").click(async function(){
-
-    window.location.href = 'https://apps.apple.com/us/app/webxr-viewer/id1295998056';
-
-})
-
-$('#copy-link-button').click(function() {
-
-    // Copy current URL to clickboard
-    const currentUrl = window.location.href;
-    navigator.clipboard.writeText(currentUrl);
-
-    // Shows user that the text has been copied
-    $('#copy-link-button').text("COPIED!");
-    $('#copy-link-button').css('background-color', 'green');
-
-    // Change text back
-    setTimeout(function() {
-        $('#copy-link-button').text('Click Here to Copy URL');
-        $('#copy-link-button').css('background-color', 'var(--light-blue)');
-    }, 1500);  
-});  
 
 /**
  * Start AR button click.
